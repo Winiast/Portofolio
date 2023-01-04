ddecd039fa488672e91937b0b20631e9b338f11d
Gustavo <gustavo.winiast@gmail.com>
2023-01-04 18:29:33 -0300

    Atualização criando rotas

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  ,
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/skills",
+    element: <Skills />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
