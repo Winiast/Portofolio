6ec168179b9c3b1489396ba3f2daddf3837c4d44
Gustavo <gustavo.winiast@gmail.com>
2023-01-13 20:13:03 -0300

    Atualizadao das rotas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.querySelector("#root"));
 
-root.render(<App></App>);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
