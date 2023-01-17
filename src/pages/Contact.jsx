50fa162ce3025f1dc6f85e759cbc76ca44baa546
Gustavo <gustavo.winiast@gmail.com>
2023-01-17 14:18:13 -0300

    Criando o formulario da tela de contato

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,26 +11,45 @@ export default function Contact() {
       <Header />
       <TextContentCenter title="Contato" />
       <div className="container-contact">
-        <h2>Parte do Mapa</h2>
         <div className="container-info">
-          <DescriptionSkills title="Endereço" description="Rua dos Bobos, 0" />
-          <DescriptionSkills title="Telefone" description="(11) 11111-1111" />
-          <DescriptionSkills title="E-mail" description="gustavo.winiast" />
+          <DescriptionSkills
+            title="Endereço"
+            description="Rua dos Bobos, 0"
+            icon="BsPencil"
+          />
+          <DescriptionSkills
+            title="Telefone"
+            description="(41) 99667-2322"
+            icon="BsPencil"
+          />
+          <DescriptionSkills
+            title="E-mail"
+            description="gustavo.winiast"
+            icon="BsPencil"
+          />
         </div>
 
         <div className="container-form">
           <h2>Contate-me!</h2>
           <form>
             <div className="form-group">
-              <label htmlFor="name">Nome</label>
-              <input type="text" id="name" />
-              <label htmlFor="email">E-mail</label>
-              <input type="email" id="email" />
-              <label htmlFor="telefone" id="telefone"></label>
-              <input type="text" id="telefone" />
-              <label htmlFor="message">Mensagem</label>
-              <textarea id="message" rows="5"></textarea>
-              <button type="submit">Enviar</button>
+              <label htmlFor="name">
+                Nome
+                <input type="text" id="name" />
+              </label>
+              <label htmlFor="email">
+                E-mail
+                <input type="email" id="email" />
+              </label>
+              <label htmlFor="telefone" id="telefone">
+                Telefone
+                <input type="text" id="telefone" />
+              </label>
+              <div className="assunto">
+                <label htmlFor="message">Mensagem</label>
+                <textarea id="message" rows="5"></textarea>
+                <button type="submit">Enviar</button>
+              </div>
             </div>
           </form>
         </div>
